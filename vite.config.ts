4bd2fa0f99439442071371b26a240fb46885c23f
Naveen3873 <naveenbe3873@gmail.com>
2026-05-13 23:43:37 +0530

    for github deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [react()],
+  base: '/finance-tracker-reactjs-v1s/',
   build: {
     rollupOptions: {
       output: {
